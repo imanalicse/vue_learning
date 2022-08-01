6ebe5ef86a2b861066ec87349bd3a7a6d6ebd9ba
Iman <imanali.cse@gmail.com>
2022-08-01 22:07:47 +0600

    Initial commit with composition api

---
+++ src/views/CompositionApi.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="CompositionApi">
+    <p>{{name}}</p>
+    <button @click="handleClick">Click</button>
+  </div>
+</template>
+
+<script>
+import {ref} from "vue";
+
+export default {
+  name: 'CompositionApi',
+  components: {
+
+  },
+  mounted() {
+    console.log("mounted")
+  },
+  setup() {
+    let name = ref("Iman")
+    const handleClick = () => {
+      name.value = "Ishak"
+    }
+    return {
+      name,
+      handleClick
+    }
+  }  
+}
+</script>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="home">
+    <CompositionApi />
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import CompositionApi from '@/views/CompositionApi.vue'
+
+export default {
+  name: 'HomeView',
+  components: {
+    CompositionApi
+  }
+}
+</script>
