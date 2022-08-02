15a2b84c67c8722d1ee89eeb2bff80b874f3b07f
Iman <imanali.cse@gmail.com>
2022-08-02 09:55:41 +0600

    Composition api

--- src/views/CompositionApi.vue
+++ src/views/CompositionApi.vue
@@ -1,12 +1,27 @@
 <template>
   <div class="CompositionApi">
-    <p>{{name}}</p>
-    <button @click="handleClick">Click</button>
+    <h3>Composition API</h3>
+    <p ref="p"> My Name is {{name}}</p>
+    <input typeof="text" v-model="name" placeholder="Type Name"> <br>
+    <button @click="updateValue">Update value</button>
+    <h4>Reactive</h4>
+    <p> {{person.name}} - {{person.age}}</p>
+    <h4>Search computed property</h4>
+    <input type="text" v-model="search">
+    <p>Search term - {{ search }}</p>
+    <div v-for="name in matchingNames" :key="name">{{ name }}</div>
+    <button @click="stopWatchEvent">Stop watching</button>
   </div>
 </template>
 
 <script>
-import {ref} from "vue";
+/**
+* reactive:
+*  does not work with primitive data type
+*  Value can be assigned directly without .value operator
+*
+ */
+import {computed, reactive, ref, watch, watchEffect} from "vue";
 
 export default {
   name: 'CompositionApi',
@@ -15,16 +30,44 @@ export default {
   },
   mounted() {
     console.log("mounted")
+    this.score = 10;
   },
   setup() {
+    console.log("setup")
     let name = ref("Iman")
-    const handleClick = () => {
+    const parapraph = ref(null);
+    const person = reactive({ name: "Iman", age: 35 });
+    const updateValue = () => {
       name.value = "Ishak"
+      parapraph.value.classList.add("test");
+      console.log(parapraph.value)
+      person.name = "Mukta"
     }
+    const search = ref("")
+    const names = ref(["Rashid", "Feroza", "Iman", "Mukta", "Ishak"])
+    const matchingNames = computed(() =>{
+      return names.value.filter((name) => name.toLowerCase().includes(search.value.toLowerCase()))
+    });
+
+    const stopWatch = watch(search, () => {
+      console.log("watch run") // run when search value chane
+    })
+    const stopWatchEffect = watchEffect(() => {
+      // console.log('watchEffect run') // run initial
+      console.log('watchEffect->>', search.value) //run when search value change
+    })
+
+    const stopWatchEvent = () => {
+      stopWatch()
+      stopWatchEffect()
+    }
+
+    return { name, updateValue, parapraph, person, search, names, matchingNames, stopWatchEvent }
+  },
+  data() {
     return {
-      name,
-      handleClick
+      score: 5
     }
-  }  
+  }
 }
 </script>
