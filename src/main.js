8843475e33ef4a1780021484a2e161876b769903
Iman <imanali.cse@gmail.com>
2022-08-12 19:48:38 +0600

    component registration

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+// import Home from './components/Home';
 
-createApp(App).use(router).mount('#app')
+const app = createApp(App)
+app.use(router).mount('#app');
+// app.component('HomeComp', Home) // this is global register, can be used anywhere.
